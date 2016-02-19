72b9dceef80d28bc0eab8963770c4388e25f20e9
andela-ggikera <githinji.gikera@andela.com>
2016-02-19 13:13:59 +0300

    (chore) Add .env to gitignore, add uid generation constants to settings

--- pictor/pictor/settings.py
+++ pictor/pictor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pictor'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Photo unique id settings
+
+UID_ALPHABET = 'abcdefghijklmnopqrstuvwxyz0123456789'
+
+UID_LENGTH = 12
