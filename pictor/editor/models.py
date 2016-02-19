b07a0aac74c92bc93cf43207c091b0fecc193b0b
andela-ggikera <githinji.gikera@andela.com>
2016-02-19 12:56:21 +0300

    (fix) Add unique id generator for public id atrribute

--- pictor/editor/models.py
+++ pictor/editor/models.py
@@ -3,6 +3,9 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from time import time
+from hashids import Hashids
+from django.conf import settings
 import os
 
 
@@ -18,12 +21,24 @@ def get_photo_path(instance, filename):
     return upload_path
 
 
+def generate_uid():
+    """Generate a unique id."""
+    salt = settings.SECRET_KEY
+    min_length = settings.UID_LENGTH,
+    alphabet = settings.UID_ALPHABET
+    hashids = Hashids(salt=salt, min_length=min_length, alphabet=alphabet)
+    unique_id = hashids.encode(int(time() * 1000))
+    return unique_id
+
+
 class Photo(models.Model):
     """This model represents a photo record uploaded by the current user."""
 
-    image = models.ImageField(upload_to=get_photo_path)
-    caption = models.CharField(blank=True, maxlength=100)
+    image = models.ImageField(upload_to=get_photo_path, max_length=255)
+    public_id = models.CharField(default=generate_uid, max_length=50)
+    caption = models.CharField(blank=True, max_length=255)
+    effects = models.CharField(blank=True, max_length=255)
     date_created = models.DateTimeField(editable=False, auto_now_add=True)
-    date_modified = models.DateTimeField(editable=False, auto_now=True)
+    date_edited = models.DateTimeField(editable=False, auto_now=True)
 
     user = models.ForeignKey(User, related_name='photos')
