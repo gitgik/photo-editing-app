fc104781fa60236c09d6c4697a5e76d04fbc075d
andela-ggikera <githinji.gikera@andela.com>
2016-02-21 12:54:33 +0300

    (feature) Define logout view

--- pictor/editor/views.py
+++ pictor/editor/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""Define the pictor views."""
+import os
+import json
+from django.shortcuts import render, reverse, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View
 
-# Create your views here.
+
+class LoginRequiredMixin(object):
+    """This mixin ensures that the user is authenticated before proceeding."""
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin, self).dispatch(
+            request, *args, **kwargs)
+
+
+class LogoutView(LoginRequiredMixin, View):
+    """This View logs the authenticated user out."""
+
+    def get(self, request, *args, **kwargs):
+        """Flash user session and redirect them to login page."""
+        logout(request)
+        return redirect(reverse('pictor:index'))
