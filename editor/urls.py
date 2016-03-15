b1ce1a2407c10e657fecf294fb822772b2c123da
andela-ggikera <githinji.gikera@andela.com>
2016-03-15 15:19:31 +0300

    (chore) Add travis integration, refactor app directory boilerplate

---
+++ editor/urls.py
@@ -0,0 +1,15 @@
+"""This file defines the urls for pictor app."""
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+# from views import LoginView
+from editor import views
+
+urlpatterns = [
+    url(r'^login/', views.social_login, name='social_login'),
+    url(r'^photos/', views.PhotoListView.as_view(), name='photos'),
+    url(r'^filters/', views.filters, name="filters"),
+    url(r'^edit_photo/', views.PhotoDetailView.as_view(), name='editphoto'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', name='logout')
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
