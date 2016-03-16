bda916516dc137a3b3ce7bb38db6aae9d1057e6f
andela-ggikera <githinji.gikera@andela.com>
2016-03-16 08:58:55 +0300

    (chore) Configure cling to work with wsgi

--- pictor/wsgi.py
+++ pictor/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bucketlist.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pictor.settings")
 
 application = Cling(get_wsgi_application())
